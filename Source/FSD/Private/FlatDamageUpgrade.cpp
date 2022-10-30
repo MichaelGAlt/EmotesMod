704f29f4f5ef1f31d7a847f7bb2ef459c932b707
michaelgoldenn <95949544+michaelgoldenn@users.noreply.github.com>
2022-10-30 10:04:52 -0400

    First Commit, adding in custom replication logic

---
+++ Source/FSD/Private/FlatDamageUpgrade.cpp
@@ -0,0 +1,18 @@
+#include "FlatDamageUpgrade.h"
+#include "Templates/SubclassOf.h"
+
+class UDamageClass;
+class AFSDPlayerState;
+class AActor;
+
+FUpgradeValues UFlatDamageUpgrade::GetUpgradedValue(TSubclassOf<AActor> Item, AFSDPlayerState* Player, UDamageClass* NewDamageClass) {
+    return FUpgradeValues{};
+}
+
+UFlatDamageUpgrade::UFlatDamageUpgrade() {
+    this->Damage = 0.00f;
+    this->DamageClass = NULL;
+    this->RequiredClass = NULL;
+    this->Condition = NULL;
+}
+
